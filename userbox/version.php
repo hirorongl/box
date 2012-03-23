6ff19eeb1e568b8751faa9d1a392fce8cccaf42e
TSUCHITANI Kazuko <tsuchi000@gmail.com>
2012-03-23 19:17:26 +0900

    ログインユーザを登録する設定の場合に、OAuthでログイン　新規ユーザ登録する時にSQLエラーが発生する問題の対応

--- userbox/version.php
+++ userbox/version.php
@@ -1,4 +1,4 @@
 <?php
-$_USERBOX_CONF['version'] = '0.0.0.20120307'; // Plugin Version
+$_USERBOX_CONF['version'] = '0.0.0.20120307a'; // Plugin Version
 //Previous  $_USERBOX_CONF['version'] = '0.0.0.20120229';
 ?>
