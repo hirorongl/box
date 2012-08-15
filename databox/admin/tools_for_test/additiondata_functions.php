f584001892424a57181aca17c4315b037a5c6803
TSUCHITANI Kazuko <tsuchi000@gmail.com>
2012-08-15 17:40:46 +0900

    不足の追加属性項目データを追加するプログラム （insert_additiondata.php） 属性セットの仕様を追加

--- databox/admin/tools_for_test/additiondata_functions.php
+++ databox/admin/tools_for_test/additiondata_functions.php
@@ -14,46 +14,41 @@ function fncexec (
     $table=$_TABLES[strtoupper($pi_name).'_def_field'];
     $table1=$_TABLES[strtoupper($pi_name).'_base'];
     $table2=$_TABLES[strtoupper($pi_name).'_addition'];
-
+	
+	
+    $table=$_TABLES[strtoupper($pi_name).'_addition'];
+	
+    $table1=$_TABLES[strtoupper($pi_name).'_base'];
+    $table2=$_TABLES[strtoupper($pi_name).'_def_fieldset_assignments'];
+    $table3=$_TABLES[strtoupper($pi_name).'_def_field'];
+    $table4=$_TABLES[strtoupper($pi_name).'_addition'];
+	
+	
     $retval = '';
+	
+    //7 = 'オプションリスト';
+    //8 = 'ラジオボタンリスト';
+	
+	$sql = "INSERT INTO ".$table.LB;
+	$sql .= " ( `id` ,`field_id` ,`value` )";
+	$sql .= " SELECT t1.id, t2.field_id,";
+	$sql .= " CASE WHEN TYPE =7";
+	$sql .= " OR TYPE =8";
+	$sql .= " THEN \"0\"";
+	$sql .= " END";
+	$sql .= " FROM ".$table1." AS t1";
+	$sql .= " , ".$table2." AS t2";
+	$sql .= " , ".$table3." AS t3";
+	$sql .= " WHERE t1.fieldset_id = t2.fieldset_id";
+	$sql .= " AND t2.field_id = t3.field_id";
+	$sql .= " AND t1.id NOT";
+	$sql .= " IN (";
+	$sql .= " SELECT t4.id";
+	$sql .= " FROM ".$table4." AS t4";
+	$sql .= " WHERE t4.field_id = t2.field_id";
+	$sql .= " )";
 
-
-    $sql = "SELECT ";
-    $sql .= " field_id";
-    $sql .= " ,type";
-
-    $sql .= " FROM ";
-    $sql .= $table;
-    $sql .= " order by field_id ";
-
-    $result = DB_query ($sql);
-    $numrows = DB_numRows ($result);
-
-    if ($numrows > 0) {
-        for ($i = 0; $i < $numrows; $i++) {
-            $A = DB_fetchArray ($result);
-
-            $field_id=$A['field_id'];
-            $type=$A['type'];
-
-            $sql2="INSERT INTO ".$table2.LB;
-            $sql2.=" (`id`,`field_id`,`value`)".LB;
-            $sql2.=" SELECT id";
-            $sql2.=" ,".$field_id;
-            //7 = 'オプションリスト';
-            //8 = 'ラジオボタンリスト';
-            if ($type==7 OR $type==8){
-                $sql2.=",'0' ";
-            }else{
-                $sql2.=",'' ";
-            }
-            $sql2.=" FROM " .$table1.LB;
-            $sql2.=" where id NOT IN (select id from ".$table2.LB;
-            $sql2.=" where field_id=".$field_id.")".LB;
-            DB_query($sql2);
-        }
-
-    }
+    DB_query($sql);
 
     $rt=" finish! ";
     return $rt;
@@ -64,10 +59,6 @@ function fncDisply(
     $pi_name
 )
 {
-
-    $table=$_TABLES[strtoupper($pi_name).'_def_field'];
-    $table1=$_TABLES[strtoupper($pi_name).'_base'];
-    $table2=$_TABLES[strtoupper($pi_name).'_addition'];
 
     $retval = "";
     $retval .= "<!DOCTYPE    HTML PUBLIC '-//W3C//DTD HTML   4.01 Transitional//EN'>".LB;
