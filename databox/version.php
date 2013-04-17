5933793074da76b2adf8c118123e4331ec4828a1
TSUCHITANI Kazuko <tsuchi000@gmail.com>
2013-04-17 12:08:31 +0900

    Version 0.0.0.20130416

--- databox/version.php
+++ databox/version.php
@@ -1,4 +1,4 @@
 <?php
-$_DATABOX_CONF['version'] = '0.0.0.20130411
+$_DATABOX_CONF['version'] = '0.0.0.20130416
 '; // Plugin Version
 ?>
--- userbox/version.php
+++ userbox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_USERBOX_CONF['version'] = '0.0.0.20130411'; // Plugin Version
+$_USERBOX_CONF['version'] = '0.0.0.20130416'; // Plugin Version
 ?>
