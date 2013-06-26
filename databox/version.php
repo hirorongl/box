12efbfcdb2e2f35a3484799dd3658126ec5cbafc
TSUCHITANI Kazuko <tsuchi000@gmail.com>
2013-06-26 16:19:50 +0900

    Version 0.0.0.20130626

--- databox/version.php
+++ databox/version.php
@@ -1,4 +1,4 @@
 <?php
-$_DATABOX_CONF['version'] = '0.0.0.20130625
+$_DATABOX_CONF['version'] = '0.0.0.20130626
 '; // Plugin Version
 ?>
--- userbox/version.php
+++ userbox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_USERBOX_CONF['version'] = '0.0.0.20130625';
+$_USERBOX_CONF['version'] = '0.0.0.20130626';
 ?>
