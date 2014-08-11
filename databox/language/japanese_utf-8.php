348833ea1112e00cf1d7db6bd4bf2ed830ba802b
土谷 和子 <tsuchi000@gmail.com>
2014-08-11 11:11:12 +0900

    DataBox Ver0.0.0.20140805c MAPS文言追加

--- databox/language/japanese_utf-8.php
+++ databox/language/japanese_utf-8.php
@@ -11,7 +11,7 @@
 ###############################################################################
 # plugins/databox/language/japanese_utf-8.php
 # もし万一エンコードの種類が　UTF-8でない場合は、utf-8に変換してください。
-# Last Update 20140805
+# Last Update 20140811
 
 ###############################################################################
 ## 管理画面 menu
@@ -310,7 +310,15 @@ $LANG_DATABOX_ADMIN['mapsmarkersclearmsg'] = "
 ";
 $LANG_DATABOX_ADMIN['mapsmarkersupdatemsg'] = "
 アトリビュートに登録されている特定のマップのマーカーをクリアし
-Dataの内容で作成します。
+データの内容で作成します。<br".XHTML.">
+logs/databox_mapsupdate.log　にログが記録されます。
+";
+$LANG_DATABOX_ADMIN['mapsmarkersupdateend'] = "
+正常 %d 件 エラー %d 件
+";
+
+$LANG_DATABOX_ADMIN['mapsmarkersupdateend2'] = "
+  (緯度・経度 計算 %d 件 更新 %d 件)
 ";
 
 //csv
--- databox/version.php
+++ databox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_DATABOX_CONF['version'] = '0.0.0.20140805b';
+$_DATABOX_CONF['version'] = '0.0.0.20140805c';
 ?>
