41de8e754a8be1fa2123865f257cce646c992176
土谷 和子 <tsuchi000@gmail.com>
2015-04-21 00:01:58 +0900

    Ver 0.0.0.20150420#207: Geeklog 標準cache対応 キャッシュから表示、キャッシュ作成

--- databox/version.php
+++ databox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_DATABOX_CONF['version'] = '0.0.0.20150417';
+$_DATABOX_CONF['version'] = '0.0.0.20150420';
 ?>
--- userbox/version.php
+++ userbox/version.php
@@ -1,3 +1,3 @@
 <?php
-$_USERBOX_CONF['version'] = '0.0.0.20150417';
+$_USERBOX_CONF['version'] = '0.0.0.20150420';
 ?>
