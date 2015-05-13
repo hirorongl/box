35c163110e4874dd19ae7047ba2b75060c68192c
土谷 和子 <tsuchi000@gmail.com>
2015-05-13 13:57:01 +0900

    DataBox Ver 0.0.0.20150423c リストア ファイル名入力をファイル選択に変更

--- databox/language/japanese_utf-8.php
+++ databox/language/japanese_utf-8.php
@@ -11,7 +11,7 @@
 ###############################################################################
 # plugins/databox/language/japanese_utf-8.php
 # もし万一エンコードの種類が　UTF-8でない場合は、utf-8に変換してください。
-# Last Update 20150304
+# Last Update 20150513
 
 ###############################################################################
 ## 管理画面 menu
@@ -305,8 +305,7 @@ $LANG_DATABOX_ADMIN['backupmsg'] =
 アップロードファイルは別途バックアップしてください。<br".XHTML.">
 ";
 $LANG_DATABOX_ADMIN['restoremsg'] = 
-"{$_CONF['backup_path']}"."databox/にある"
-."ファイル名を指定してください。（省略時databox.xml）<br".XHTML.">
+"リストアするファイルを選択してください。<br".XHTML.">
 DataBox のデータベースデータをリストアします。<br".XHTML.">
 アップロードファイルは別途もどしてください。<br".XHTML.">
 ";
